b0b3ccc4f7fe4dec38b190dec4b288b452ef15db
Finyahrua <finaddy@gmail.com>
2022-08-10 08:07:17 +0300

    added the subtitle prop to the title component and created the footer component

--- src/components/Titles/Titles.js
+++ src/components/Titles/Titles.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Titles.css";
 
-function Title({ title }) {
+function Title({ title, subtitle }) {
   return (
-    <div className="container">
-      <img
-        src="https://github.com/amawalla/js-coding-challenge/blob/master/assets/placeholder.png?raw=true"
-        alt="img"
-        
-      />
-      <h1 className="titles">{title}</h1>
+    <div>
+      <div className="container">
+        <img
+          src="https://github.com/amawalla/js-coding-challenge/blob/master/assets/placeholder.png?raw=true"
+          alt="img"
+        />
+        <h1 className="titles">{title}</h1>
+      </div>
+      <p className="sub-title">{subtitle}</p>
     </div>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import Title from "../components/Titles/Titles";
 
@@ -8,10 +9,11 @@ function HomeScreen() {
     <div>
       <NavBar />
       <Banner />
-      <div style={{  display: "flex",padding: "0 10rem"}}>
-        <Title title="SERIES" />
-        <Title title="MOVIES" />
+      <div style={{ display: "flex", padding: "0 10rem" }}>
+        <Title title="SERIES" subtitle="Popular Series" />
+        <Title title="MOVIES" subtitle="Popular Movies" />
       </div>
+      <Footer />
     </div>
   );
 }
