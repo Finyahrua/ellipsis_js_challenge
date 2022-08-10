b4301236f618b014a342b58f1c07cb579be5a640
Finyahrua <finaddy@gmail.com>
2022-08-10 06:20:06 +0300

    done with the 'popular titles' banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h3>Popular Titles</h3>
+    </div>
+  );
+}
+
+export default Banner;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import NavBar from '../components/NavBar/NavBar'
-
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import NavBar from "../components/NavBar/NavBar";
 
 function HomeScreen() {
   return (
-    <div><NavBar/></div>
-  )
+    <div>
+      <NavBar />
+      <Banner />
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
